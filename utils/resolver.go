375414c1225c23a9f21e2f2c7c69b69be2c24c2e
Angus Lees <gus@inodes.org>
2018-09-02 21:12:03 +1000

    Restore `defaultRegistry`
    Oops, turns out it _is_ still used.

--- utils/resolver.go
+++ utils/resolver.go
@@ -23,6 +23,8 @@ import (
 	"github.com/genuinetools/reg/repoutils"
 )
 
+const defaultRegistry = "registry-1.docker.io"	
+
 // ImageName represents the parts of a docker image name
 type ImageName struct {
 	// eg: "myregistryhost:5000/fedora/httpd:version1.0"
