a226fa64b90a86c44a5ae4f0fbedb3c25eab36c1
Cam Hutchison <camh@xdna.net>
2020-12-10 11:45:57 +1100

    Bump go-jsonnet dep to v0.17.0
    Upgrade the version of go-jsonnet used by kubecfg to v0.17.0 from
    v0.15.0:
    
        go get github.com/google/go-jsonnet@v0.17.0
        make vendor
    
    go-jsonnet v0.15.0 does not build on 32-bit machines due to
    https://github.com/google/go-jsonnet/issues/376 (constant overflows
    int). This was fixed in https://github.com/google/go-jsonnet/pull/377
    and released in v0.16.0. The current release of go-jsonnet is v0.17.0

--- vendor/github.com/google/go-jsonnet/vm.go
+++ vendor/github.com/google/go-jsonnet/vm.go
@@ -19,9 +19,14 @@ package jsonnet
 import (
 	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 	"runtime/debug"
+	"sort"
+	"strings"
 
 	"github.com/google/go-jsonnet/ast"
+	"github.com/google/go-jsonnet/internal/parser"
 	"github.com/google/go-jsonnet/internal/program"
 )
 
@@ -125,7 +130,7 @@ const (
 )
 
 // version is the current gojsonnet's version
-const version = "v0.15.0"
+const version = "v0.17.0"
 
 // Evaluate evaluates a Jsonnet program given by an Abstract Syntax Tree
 // and returns serialized JSON as string.
@@ -141,7 +146,7 @@ func (vm *VM) Evaluate(node ast.Node) (val string, err error) {
 
 // EvaluateStream evaluates a Jsonnet program given by an Abstract Syntax Tree
 // and returns an array of JSON strings.
-func (vm *VM) EvaluateStream(node ast.Node) (output interface{}, err error) {
+func (vm *VM) EvaluateStream(node ast.Node) (output []string, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			err = fmt.Errorf("(CRASH) %v\n%s", r, debug.Stack())
@@ -153,7 +158,7 @@ func (vm *VM) EvaluateStream(node ast.Node) (output interface{}, err error) {
 // EvaluateMulti evaluates a Jsonnet program given by an Abstract Syntax Tree
 // and returns key-value pairs.
 // The keys are strings and the values are JSON strigns (serialized JSON).
-func (vm *VM) EvaluateMulti(node ast.Node) (output interface{}, err error) {
+func (vm *VM) EvaluateMulti(node ast.Node) (output map[string]string, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			err = fmt.Errorf("(CRASH) %v\n%s", r, debug.Stack())
@@ -162,13 +167,13 @@ func (vm *VM) EvaluateMulti(node ast.Node) (output interface{}, err error) {
 	return evaluateMulti(node, vm.ext, vm.tla, vm.nativeFuncs, vm.MaxStack, vm.importCache, vm.StringOutput)
 }
 
-func (vm *VM) evaluateSnippet(filename string, snippet string, kind evalKind) (output interface{}, err error) {
+func (vm *VM) evaluateSnippet(diagnosticFileName ast.DiagnosticFileName, filename string, snippet string, kind evalKind) (output interface{}, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			err = fmt.Errorf("(CRASH) %v\n%s", r, debug.Stack())
 		}
 	}()
-	node, err := SnippetToAST(filename, snippet)
+	node, err := program.SnippetToAST(diagnosticFileName, filename, snippet)
 	if err != nil {
 		return "", err
 	}
@@ -186,12 +191,85 @@ func (vm *VM) evaluateSnippet(filename string, snippet string, kind evalKind) (o
 	return output, nil
 }
 
+func getAbsPath(path string) (string, error) {
+	var absPath string
+	if filepath.IsAbs(path) {
+		absPath = path
+	} else {
+		wd, err := os.Getwd()
+		if err != nil {
+			return "", nil
+		}
+		absPath = strings.Join([]string{wd, path}, string(filepath.Separator))
+	}
+	cleanedAbsPath, err := filepath.EvalSymlinks(absPath)
+	if err != nil {
+		return "", err
+	}
+	return cleanedAbsPath, nil
+}
+
+func (vm *VM) findDependencies(filePath string, node *ast.Node, dependencies map[string]struct{}, stackTrace *[]traceFrame) (err error) {
+	var cleanedAbsPath string
+	switch i := (*node).(type) {
+	case *ast.Import:
+		node, foundAt, err := vm.ImportAST(filePath, i.File.Value)
+		if err != nil {
+			*stackTrace = append([]traceFrame{{Loc: *i.Loc()}}, *stackTrace...)
+			return err
+		}
+		cleanedAbsPath = foundAt
+		if _, isFileImporter := vm.importer.(*FileImporter); isFileImporter {
+			cleanedAbsPath, err = getAbsPath(foundAt)
+			if err != nil {
+				*stackTrace = append([]traceFrame{{Loc: *i.Loc()}}, *stackTrace...)
+				return err
+			}
+		}
+		// Check that we haven't already parsed the imported file.
+		if _, alreadyParsed := dependencies[cleanedAbsPath]; alreadyParsed {
+			return nil
+		}
+		dependencies[cleanedAbsPath] = struct{}{}
+		err = vm.findDependencies(foundAt, &node, dependencies, stackTrace)
+		if err != nil {
+			*stackTrace = append([]traceFrame{{Loc: *i.Loc()}}, *stackTrace...)
+			return err
+		}
+	case *ast.ImportStr:
+		foundAt, err := vm.ResolveImport(filePath, i.File.Value)
+		if err != nil {
+			*stackTrace = append([]traceFrame{{Loc: *i.Loc()}}, *stackTrace...)
+			return err
+		}
+		cleanedAbsPath = foundAt
+		if _, isFileImporter := vm.importer.(*FileImporter); isFileImporter {
+			cleanedAbsPath, err = getAbsPath(foundAt)
+			if err != nil {
+				*stackTrace = append([]traceFrame{{Loc: *i.Loc()}}, *stackTrace...)
+				return err
+			}
+		}
+		dependencies[cleanedAbsPath] = struct{}{}
+	default:
+		for _, node := range parser.Children(i) {
+			err = vm.findDependencies(filePath, &node, dependencies, stackTrace)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return
+}
+
 // EvaluateSnippet evaluates a string containing Jsonnet code, return a JSON
 // string.
 //
-// The filename parameter is only used for error messages.
+// The filename parameter is used for resolving relative imports and for errors messages.
+//
+// Deprecated: Use EvaluateFile or EvaluateAnonymousSnippet instead.
 func (vm *VM) EvaluateSnippet(filename string, snippet string) (json string, formattedErr error) {
-	output, err := vm.evaluateSnippet(filename, snippet, evalKindRegular)
+	output, err := vm.evaluateSnippet(ast.DiagnosticFileName(filename), filename, snippet, evalKindRegular)
 	if err != nil {
 		return "", errors.New(vm.ErrorFormatter.Format(err))
 	}
@@ -202,9 +280,11 @@ func (vm *VM) EvaluateSnippet(filename string, snippet string) (json string, for
 // EvaluateSnippetStream evaluates a string containing Jsonnet code to an array.
 // The array is returned as an array of JSON strings.
 //
-// The filename parameter is only used for error messages.
+// The filename parameter is used for resolving relative imports and for errors messages.
+//
+// Deprecated: Use EvaluateFileStream or EvaluateAnonymousSnippetStream instead.
 func (vm *VM) EvaluateSnippetStream(filename string, snippet string) (docs []string, formattedErr error) {
-	output, err := vm.evaluateSnippet(filename, snippet, evalKindStream)
+	output, err := vm.evaluateSnippet(ast.DiagnosticFileName(filename), filename, snippet, evalKindStream)
 	if err != nil {
 		return nil, errors.New(vm.ErrorFormatter.Format(err))
 	}
@@ -215,14 +295,157 @@ func (vm *VM) EvaluateSnippetStream(filename string, snippet string) (docs []str
 // EvaluateSnippetMulti evaluates a string containing Jsonnet code to key-value
 // pairs. The keys are field name strings and the values are JSON strings.
 //
-// The filename parameter is only used for error messages.
+// The filename parameter is used for resolving relative imports and for errors messages.
+//
+// Deprecated: Use EvaluateFileMulti or EvaluateAnonymousSnippetMulti instead.
 func (vm *VM) EvaluateSnippetMulti(filename string, snippet string) (files map[string]string, formattedErr error) {
-	output, err := vm.evaluateSnippet(filename, snippet, evalKindMulti)
+	output, err := vm.evaluateSnippet(ast.DiagnosticFileName(filename), filename, snippet, evalKindMulti)
 	if err != nil {
 		return nil, errors.New(vm.ErrorFormatter.Format(err))
 	}
 	files = output.(map[string]string)
 	return
+}
+
+// EvaluateAnonymousSnippet evaluates a string containing Jsonnet code, return a JSON
+// string.
+//
+// The filename parameter is only used for error messages.
+func (vm *VM) EvaluateAnonymousSnippet(filename string, snippet string) (json string, formattedErr error) {
+	output, err := vm.evaluateSnippet(ast.DiagnosticFileName(filename), "", snippet, evalKindRegular)
+	if err != nil {
+		return "", errors.New(vm.ErrorFormatter.Format(err))
+	}
+	json = output.(string)
+	return
+}
+
+// EvaluateAnonymousSnippetStream evaluates a string containing Jsonnet code to an array.
+// The array is returned as an array of JSON strings.
+//
+// The filename parameter is only used for error messages.
+func (vm *VM) EvaluateAnonymousSnippetStream(filename string, snippet string) (docs []string, formattedErr error) {
+	output, err := vm.evaluateSnippet(ast.DiagnosticFileName(filename), "", snippet, evalKindStream)
+	if err != nil {
+		return nil, errors.New(vm.ErrorFormatter.Format(err))
+	}
+	docs = output.([]string)
+	return
+}
+
+// EvaluateAnonymousSnippetMulti evaluates a string containing Jsonnet code to key-value
+// pairs. The keys are field name strings and the values are JSON strings.
+//
+// The filename parameter is only used for error messages.
+func (vm *VM) EvaluateAnonymousSnippetMulti(filename string, snippet string) (files map[string]string, formattedErr error) {
+	output, err := vm.evaluateSnippet(ast.DiagnosticFileName(filename), "", snippet, evalKindMulti)
+	if err != nil {
+		return nil, errors.New(vm.ErrorFormatter.Format(err))
+	}
+	files = output.(map[string]string)
+	return
+}
+
+// EvaluateFile evaluates Jsonnet code in a file and returns a JSON
+// string.
+//
+// The importer is used to fetch the contents of the file.
+func (vm *VM) EvaluateFile(filename string) (json string, formattedErr error) {
+	node, _, err := vm.ImportAST("", filename)
+	if err != nil {
+		return "", errors.New(vm.ErrorFormatter.Format(err))
+	}
+	output, err := vm.Evaluate(node)
+	if err != nil {
+		return "", errors.New(vm.ErrorFormatter.Format(err))
+	}
+	return output, nil
+}
+
+// EvaluateFileStream evaluates Jsonnet code in a file to an array.
+// The array is returned as an array of JSON strings.
+//
+// The importer is used to fetch the contents of the file.
+func (vm *VM) EvaluateFileStream(filename string) (docs []string, formattedErr error) {
+	node, _, err := vm.ImportAST("", filename)
+	if err != nil {
+		return nil, errors.New(vm.ErrorFormatter.Format(err))
+	}
+	output, err := vm.EvaluateStream(node)
+	if err != nil {
+		return nil, errors.New(vm.ErrorFormatter.Format(err))
+	}
+	return output, nil
+}
+
+// EvaluateFileMulti evaluates Jsonnet code in a file to key-value
+// pairs. The keys are field name strings and the values are JSON strings.
+//
+// The importer is used to fetch the contents of the file.
+func (vm *VM) EvaluateFileMulti(filename string) (files map[string]string, formattedErr error) {
+	node, _, err := vm.ImportAST("", filename)
+	if err != nil {
+		return nil, errors.New(vm.ErrorFormatter.Format(err))
+	}
+	output, err := vm.EvaluateMulti(node)
+	if err != nil {
+		return nil, errors.New(vm.ErrorFormatter.Format(err))
+	}
+	return output, nil
+}
+
+// FindDependencies returns a sorted array of unique transitive dependencies (via import or importstr)
+// from all the given `importedPaths` which are themselves excluded from the returned array.
+// The `importedPaths` are parsed as if they were imported from a Jsonnet file located at `importedFrom`.
+func (vm *VM) FindDependencies(importedFrom string, importedPaths []string) ([]string, error) {
+	var nodes []*ast.Node
+	var stackTrace []traceFrame
+	filePaths := make([]string, len(importedPaths))
+	depsToExclude := make([]string, len(importedPaths))
+	deps := make(map[string]struct{})
+
+	for i, filePath := range importedPaths {
+		node, foundAt, err := vm.ImportAST(importedFrom, filePath)
+		if err != nil {
+			return nil, err
+		}
+		cleanedAbsPath := foundAt
+		if _, isFileImporter := vm.importer.(*FileImporter); isFileImporter {
+			cleanedAbsPath, err = getAbsPath(foundAt)
+			if err != nil {
+				return nil, err
+			}
+		}
+		nodes = append(nodes, &node)
+		filePaths[i] = foundAt
+
+		// Add `importedPaths` to the dependencies so that they are not parsed again.
+		// Will be removed before returning.
+		deps[cleanedAbsPath] = struct{}{}
+		depsToExclude[i] = cleanedAbsPath
+	}
+
+	for i, filePath := range filePaths {
+		err := vm.findDependencies(filePath, nodes[i], deps, &stackTrace)
+		if err != nil {
+			err = makeRuntimeError(err.Error(), stackTrace)
+			return nil, errors.New(vm.ErrorFormatter.Format(err))
+		}
+	}
+
+	// Exclude `importedPaths` from the dependencies.
+	for _, dep := range depsToExclude {
+		delete(deps, dep)
+	}
+
+	dependencies, i := make([]string, len(deps)), 0
+	for key := range deps {
+		dependencies[i] = key
+		i++
+	}
+	sort.Strings(dependencies)
+
+	return dependencies, nil
 }
 
 // ResolveImport finds the actual path where the imported file can be found.
@@ -251,7 +474,7 @@ func (vm *VM) ImportAST(importedFrom, importedPath string) (contents ast.Node, f
 
 // SnippetToAST parses a snippet and returns the resulting AST.
 func SnippetToAST(filename string, snippet string) (ast.Node, error) {
-	return program.SnippetToAST(filename, snippet)
+	return program.SnippetToAST(ast.DiagnosticFileName(filename), filename, snippet)
 }
 
 // Version returns the Jsonnet version number.
